090a869b90f89726957e131ba01ae9bfb5bda982
dkanampa <dkanampa@ncsu.edu>
2017-04-20 12:53:01 -0400

    Add files vai upload
    This update includes a rough version of the interface enough for coding to begin. Interface will continue to be updated as coding process begins.

--- Major Project 3/Major Project 3/Main Form.vb
+++ Major Project 3/Major Project 3/Main Form.vb
@@ -1,3 +1,16 @@
 ﻿Public Class MainForm
+    Private Sub ProductBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles ProductBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ProductBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.ProductDatabaseDataSet)
 
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'ProductDatabaseDataSet.Vendor' table. You can move, or remove it, as needed.
+        Me.VendorTableAdapter.Fill(Me.ProductDatabaseDataSet.Vendor)
+        'TODO: This line of code loads data into the 'ProductDatabaseDataSet.Product' table. You can move, or remove it, as needed.
+        Me.ProductTableAdapter.Fill(Me.ProductDatabaseDataSet.Product)
+
+    End Sub
 End Class
